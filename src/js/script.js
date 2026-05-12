83085557970433a5ddb8d60191d4be036fc76d91
abdumalik0099 <kachok477@gmail.com>
2026-05-12 22:32:37 +0500

    Loyiha yakunlandi va gitignore sozlandi

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ const exploreOverlay = document.getElementById('exploreOverlay');
 const exploreLink   = document.querySelector('.header__menu-link--dropdown');
 const searchInput   = document.querySelector('.header__search-input');
 const modalSearch   = document.querySelector('.explore-modal__searchbar input');
+const burger = document.querySelector('.header__burger')
+
 
 function openModal() {
   exploreModal.classList.add('active');
@@ -15,6 +17,12 @@ function closeModal() {
   exploreOverlay.classList.remove('active');
   document.body.style.overflow = '';
 }
+
+burger?.addEventListener('click', () => {
+  exploreModal.classList.toggle('active');
+  exploreOverlay.classList.toggle('active');
+  document.body.classList.toggle('active');
+});
 
 // Explore bosganda
 exploreLink?.addEventListener('click', (e) => {
@@ -56,6 +64,9 @@ const loginForm    = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
 const footerText   = document.getElementById('authFooterText');
 const switchToReg  = document.getElementById('switchToRegister');
+const modal = document.querySelector('.header__profile-mobile');
+
+
 
 function openAuth(mode = 'login') {
   authModal.classList.add('active');
@@ -87,7 +98,11 @@ function setMode(mode) {
     setMode(mode === 'login' ? 'register' : 'login');
   });
 }
-
+modal?.addEventListener('click', () => {
+  authModal.classList.toggle('active');
+  authOverlay.classList.toggle('active');
+  document.body.classList.toggle('active');
+});
 loginBtn?.addEventListener('click',  (e) => { e.preventDefault(); openAuth('login'); });
 signupBtn?.addEventListener('click', (e) => { e.preventDefault(); openAuth('register'); });
 authClose?.addEventListener('click', closeAuth);
